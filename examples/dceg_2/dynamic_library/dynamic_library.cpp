08e2d89be0c1907c6897fab2b160b9302675d604
Joshion <joshion@outlook.com>
2022-08-19 08:56:42 +0800

    Refactor

---
+++ examples/dceg_2/dynamic_library/dynamic_library.cpp
@@ -0,0 +1,14 @@
+#include "dynamic_library.hpp"
+
+int func()
+{
+    return 0;
+}
+
+MyClass::MyClass(/* args */)
+{
+}
+
+MyClass::~MyClass()
+{
+}
---
+++ examples/dceg_2/executable/main.cpp
@@ -0,0 +1,8 @@
+#include "dynamic_library.hpp"
+
+int main()
+{
+    MyClass myclass;
+    int i = func();
+    return 0;
+}
